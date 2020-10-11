9731db44ed17cbd04ce2efe1a0774ca5b47ea4c4
NaeemMeman <naeemmeman@gmail.com>
2020-10-11 01:23:46 -0400

    some setup

--- src/index.js
+++ src/index.js
@@ -1,17 +1,89 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+class Form extends React.Component {
+    render() {
+        return (
+            <div class="form-container">
+                <div class="column-container">
+                    <Cart/>
+                </div>
+                <div class="column-container">
+                    <Billing/>
+                    <Payment/>
+                </div>
+            </div>
+        );
+    }
+}
+
+class Cart extends React.Component {
+    render() {
+        return (
+            <div class="green">
+                <Items/>
+            </div>
+        );
+    }
+}
+
+class Billing extends React.Component {
+    render() {
+        return (
+            <div class="red">
+                Red
+            </div>
+        );
+    }
+}
+
+class Payment extends React.Component {
+    render() {
+        return (
+            <div class="blue">
+                Blue
+            </div>
+        );
+    }
+}
+
+class Items extends React.Component {
+    render() {
+        return (
+            <div>
+                <ul>
+                    <li>
+                        <Item/>
+                    </li>
+                    <li>
+                        <Item/>
+                    </li>
+                    <li>
+                        <Item/>
+                    </li>
+                    <li>
+                        <Item/>
+                    </li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+class Item extends React.Component {
+    render() {
+        return (
+            <div>
+                Item
+            </div>
+        );
+    }
+}
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Form />,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
